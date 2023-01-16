c0a83e9b8ba0fcaecb17e7dd81b2c9517be6cac4
vitorr7df <vitor.rochadf@gmail.com>
2023-01-15 22:11:43 -0300

    Inserindo Routes

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <Box v-if="listaEstaVazia">
-          Confie em si mesmo ;)
-        </Box>
-      </div>
+      <RouterView></RouterView>
     </div>
   </main>
 </template>
@@ -18,34 +12,19 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue'
-import Formulario from './components/Formulario.vue'
-import Tarefa from './components/Tarefa.vue'
-import Box from './components/Box.vue'
-import ITarefa from './interfaces/ITarefa'
+
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    Formulario,
-    Tarefa,
-    Box
   },
   data () {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
-  computed: {
-    listaEstaVazia () : boolean {
-      return this.tarefas.length === 0
-    }
-  },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
-    trocarTema (modoEscuroAtivo: boolean) {
+    trocarTema (modoEscuroAtivo: boolean) : void {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
   }
--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,39 +1,57 @@
 <template>
-    <header>
-        <h1>
-            <img src="../assets/logo.png" alt="">
-        </h1>
-        <button class="button" @click="alterarTema">
-          {{ textoBotao }}
-        </button>
-    </header>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="">
+    </h1>
+    <div class="has-text-centered">
+      <button class="button" @click="alterarTema">
+        {{ textoBotao }}
+      </button>
+    </div>
+    <nav class="panel at-5">
+      <ul>
+        <li>
+          <RouterLink to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'BarraLateral',
-    emits: ['aoTemaAlterado'],
-    data () {
-      return {
-        modoEscuroAtivo: false
-      }
-    },
-    computed: {
-      textoBotao () {
-        if (this.modoEscuroAtivo) {
-          return 'Desativar modo escuro'
-        }
-        return 'Ativar modo escuro'
-      }
-    },
-    methods: {
-      alterarTema () {
-        this.modoEscuroAtivo = !this.modoEscuroAtivo
-        this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
-      }
+  name: 'BarraLateral',
+  emits: ['aoTemaAlterado'],
+  data() {
+    return {
+      modoEscuroAtivo: false
     }
+  },
+  computed: {
+    textoBotao() {
+      if (this.modoEscuroAtivo) {
+        return 'Desativar modo escuro'
+      }
+      return 'Ativar modo escuro'
+    }
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo
+      this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+    }
+  }
 })
 </script>
 
@@ -46,10 +64,26 @@ header {
   height: 100vh;
   text-align: center;
 }
+
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
 }
+  .panel li {
+    margin: 8px 0;
+  }
+
+  .link {
+    color: #fff;
+  }
+
+  .link:hover {
+    color: #FAF0CA;
+  }
+
+  .link.router-link-active {
+    color: #FAF0CA;
+  }
 </style>
